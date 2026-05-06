3bf24d7621d915eb7802bb61aa39c09caff9e089
Mirko Lenz <mirko@mirkolenz.com>
2026-05-06 07:41:10 +0200

    fix(pkgs): remove direnv override

--- pkgs/by-name/direnv.nix
+++
@@ -1,6 +0,0 @@
-{ prev, stdenv }:
-prev.direnv.overrideAttrs (prevAttrs: {
-  # https://github.com/NixOS/nixpkgs/pull/513081
-  # https://github.com/NixOS/nixpkgs/issues/208951
-  doCheck = !stdenv.hostPlatform.isDarwin;
-})
