36f6645990bb6d6a7fbfdc2a994a14fc1409734a
Mirko Lenz <mirko@mirkolenz.com>
2026-05-12 12:15:57 +0200

    fix(linux): disable firewalld

--- system/linux-base/network.nix
+++ system/linux-base/network.nix
@@ -1,4 +1,4 @@
-{ config, lib, ... }:
+{ lib, ... }:
 {
   networking = {
     useNetworkd = true;
@@ -8,8 +8,6 @@
     useHostResolvConf = false;
     nftables.enable = true;
   };
-
-  services.firewalld.enable = config.networking.firewall.enable;
 
   # Do not manage wifi interfaces with networkd by default
   systemd.network.networks."90-wlan" = {
