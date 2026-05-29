e2964d8d4cbf095d73c3fafb9f186f20ed693329
Mirko Lenz <mirko@mirkolenz.com>
2026-05-29 13:45:47 +0200

    chore(herdr): update settings

--- home/mlenz/common/herdr.nix
+++ home/mlenz/common/herdr.nix
@@ -6,11 +6,30 @@
     # https://herdr.dev/docs/configuration/
     settings = {
       onboarding = false;
-      theme.name = "terrminal";
+      theme.name = "gruvbox";
+      terminal = {
+        default_shell = "fish";
+        new_cwd = "follow";
+      };
+      keys = {
+        prefix = "ctrl+b";
+        command = [
+          {
+            key = "prefix+alt+g";
+            type = "pane";
+            command = "lazygit";
+          }
+        ];
+      };
       ui = {
         toast.delivery = "terminal";
-        sound.enabled = true;
+        sound.enabled = false;
+        agent_panel_scope = "all";
         show_agent_labels_on_pane_borders = true;
+        prompt_new_tab_name = true;
+      };
+      session = {
+        resume_agents_on_restore = false;
       };
     };
   };
