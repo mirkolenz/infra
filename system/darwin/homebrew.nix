6a72f15b05f88f2274ab06a4f369648f2912aa27
Mirko Lenz <mirko@mirkolenz.com>
2026-06-04 10:11:47 +0200

    chore(brew): disable cleanup

--- system/darwin/homebrew.nix
+++ system/darwin/homebrew.nix
@@ -170,7 +170,7 @@ in
     global.autoUpdate = true;
     onActivation = {
       autoUpdate = true;
-      cleanup = "uninstall";
+      # cleanup = "uninstall"; # todo: broken as of 2026-06-04
       upgrade = true;
       extraEnv = {
         HOMEBREW_NO_ENV_HINTS = "1";
