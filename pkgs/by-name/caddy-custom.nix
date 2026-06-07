611b61822da194724094fb14101a0ce98a7e3f75
Mirko Lenz <mirko@mirkolenz.com>
2026-06-07 16:55:16 +0200

    chore(pkgs): auto-fix hashes

--- pkgs/by-name/caddy-custom.nix
+++ pkgs/by-name/caddy-custom.nix
@@ -4,5 +4,5 @@ nixpkgs.caddy.withPlugins {
     # https://github.com/caddy-dns/cloudflare/tags
     "github.com/caddy-dns/cloudflare@v0.2.4"
   ];
-  hash = "sha256-bzMqxWTqrJ1skZmRTXyEMCKStXpljbqe5r0Ve2cnBfM=";
+  hash = "sha256-8yZDrejNKsaUnUaTUFYbarWNmxafqp2z2rWo+XRsxV8=";
 }
