4fb07a0c8f9d583d6a806a7356e729874033c32e
Mirko Lenz <mirko@mirkolenz.com>
2026-06-18 13:12:37 +0200

    chore(llama-cpp): update models

--- modules/services/llama-cpp.nix
+++ modules/services/llama-cpp.nix
@@ -10,12 +10,12 @@
           cache-ram = 0; # unified memory
           cache-type-k = "q8_0";
           cache-type-v = "q8_0";
-          ctx-size = 128 * 1024 * parallel;
+          ctx-size = 256 * 1024 * parallel;
           flash-attn = "on";
           mlock = true;
           mmap = false;
           n-gpu-layers = "all";
-          parallel = 2;
+          parallel = 1;
           sleep-idle-seconds = -1;
           # keep-sorted end
         };
@@ -34,18 +34,6 @@
         "qwen3.6-35b-a3b" = {
           # keep-sorted start
           hf-repo = "unsloth/Qwen3.6-35B-A3B-MTP-GGUF:UD-Q4_K_XL";
-          min-p = 0.00;
-          reasoning = "on";
-          spec-draft-n-max = 4;
-          spec-type = "draft-mtp";
-          temperature = 1.0;
-          top-k = 20;
-          top-p = 0.95;
-          # keep-sorted end
-        };
-        "qwen3.6-27b" = {
-          # keep-sorted start
-          hf-repo = "unsloth/Qwen3.6-27b-MTP-GGUF:UD-Q4_K_XL";
           min-p = 0.00;
           reasoning = "on";
           spec-draft-n-max = 4;
