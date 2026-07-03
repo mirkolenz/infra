0f557ba65e6fcdb0ea0d52cad4343c919f04441b
Mirko Lenz <mirko@mirkolenz.com>
2026-07-03 11:11:09 +0200

    chore(pkgs/hotfixes): update

--- pkgs/overrides/hotfixes.nix
+++ pkgs/overrides/hotfixes.nix
@@ -3,9 +3,4 @@ final: prev:
 // (prev.lib.optionalAttrs prev.stdenv.hostPlatform.isLinux {
 })
 // (prev.lib.optionalAttrs prev.stdenv.hostPlatform.isDarwin {
-  llama-cpp = final.lib'.disableUpdateScript (
-    prev.llama-cpp.override {
-      nodejs_latest = prev.nodejs;
-    }
-  );
 })
