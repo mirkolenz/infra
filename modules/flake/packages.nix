12c4243cad29eb74a1602daaf81c8c1caa0c6875
Mirko Lenz <mirko@mirkolenz.com>
2026-07-06 09:01:55 +0200

    fix(checks): construct only from overlay, not from flake

--- modules/flake/packages.nix
+++ modules/flake/packages.nix
@@ -14,6 +14,14 @@
     let
       isAvailable =
         value: lib.meta.availableOn { inherit system; } value && !(value.meta.broken or false);
+      isHydraTarget = value: lib.elem system (value.meta.hydraPlatforms or [ system ]);
+
+      exports = lib.filterAttrs (_: isAvailable) (
+        pkgs.custom.flattenedPackages
+        // lib.optionalAttrs (system == "aarch64-linux") {
+          raspi-kernel = self.nixosConfigurations.raspi.config.boot.kernelPackages.kernel;
+        }
+      );
     in
     {
       _module.args.pkgs = import inputs.nixpkgs {
@@ -21,12 +29,8 @@
         config = self.nixpkgsConfig;
         overlays = [ self.overlays.default ];
       };
-      packages = lib.filterAttrs (_: isAvailable) (
-        pkgs.custom.flattenedPackages
-        // lib.optionalAttrs (system == "aarch64-linux") {
-          raspi-kernel = self.nixosConfigurations.raspi.config.boot.kernelPackages.kernel;
-        }
-      );
+      packages = exports;
+      checks = lib.filterAttrs (name: value: isHydraTarget value) exports;
       legacyPackages = pkgs;
     };
 }
