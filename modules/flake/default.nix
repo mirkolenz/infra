903f76830520f0cd730be2e86905bdf0e7ba1460
Mirko Lenz <mirko@mirkolenz.com>
2026-07-06 09:00:43 +0200

    refactor(flake): consolidate modules

--- modules/flake/default.nix
+++ modules/flake/default.nix
@@ -1,5 +1,17 @@
-{ inputs, ... }:
 {
+  inputs,
+  config,
+  self,
+  lib',
+  ...
+}:
+{
+  debug = true;
+  systems = [
+    "aarch64-darwin"
+    "aarch64-linux"
+    "x86_64-linux"
+  ];
   imports = [
     inputs.flake-parts.flakeModules.modules
     inputs.disko.flakeModules.default
@@ -10,4 +22,14 @@
     # flake-parts-level option declarations (e.g. configurations.{nixos,darwin,home})
     (inputs.import-tree ../../options/flake-parts)
   ];
+  flake = {
+    overlays.default = import ../../pkgs config.flake.overlayArgs;
+    nixpkgsConfig = {
+      allowUnfree = true;
+      nvidia.acceptLicense = true;
+    };
+    overlayArgs = {
+      inherit self inputs lib';
+    };
+  };
 }
--- modules/flake/systems.nix
+++
@@ -1,8 +0,0 @@
-{
-  debug = true;
-  systems = [
-    "aarch64-darwin"
-    "aarch64-linux"
-    "x86_64-linux"
-  ];
-}
