67896aa5e8fb1c43c230c862d9ea01a23e567c4a
Mirko Lenz <mirko@mirkolenz.com>
2026-07-09 19:47:15 +0200

    chore(deps/nix): update
    Flake lock file updates:
    
    • Updated input 'home-manager':
        'github:nix-community/home-manager/f4d01c1' (2026-07-08)
      → 'github:nix-community/home-manager/144f4e3' (2026-07-09)
    • Updated input 'mistral-vibe':
        'github:mistralai/mistral-vibe/474a0e4' (2026-07-03)
      → 'github:mistralai/mistral-vibe/30792a4' (2026-07-09)
    • Updated input 'nixos-hardware':
        'github:nixos/nixos-hardware/662bd6e' (2026-07-06)
      → 'github:nixos/nixos-hardware/968980f' (2026-07-09)
    • Updated input 'nixvim':
        'github:nix-community/nixvim/6d1424c' (2026-07-08)
      → 'github:nix-community/nixvim/511a763' (2026-07-09)

--- flake.nix
+++ flake.nix
@@ -63,7 +63,7 @@
     };
     import-tree.url = "github:denful/import-tree";
     mistral-vibe = {
-      url = "github:mistralai/mistral-vibe/v2.19.0";
+      url = "github:mistralai/mistral-vibe/v2.19.1";
       inputs = {
         nixpkgs.follows = "nixpkgs";
         pyproject-nix.follows = "pyproject-nix";
