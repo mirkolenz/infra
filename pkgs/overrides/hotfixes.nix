f38232b0e4c7b16730c416ee0e5dc864e42a0ed1
Mirko Lenz <mirko@mirkolenz.com>
2026-07-27 12:13:54 +0200

    chore(pkgs/hotfixes): update

--- pkgs/overrides/hotfixes.nix
+++ pkgs/overrides/hotfixes.nix
@@ -1,13 +1,6 @@
 final: prev:
-{
-  inherit (final.stable)
-    sbomnix
-    ;
-}
+{ }
 // (prev.lib.optionalAttrs prev.stdenv.hostPlatform.isLinux {
-  inherit (final.stable)
-    icloudpd
-    ;
 })
 // (prev.lib.optionalAttrs prev.stdenv.hostPlatform.isDarwin {
 })
