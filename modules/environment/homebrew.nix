f299be056389f361e537c65b93bd75d319b99669
Mirko Lenz <mirko@mirkolenz.com>
2026-07-30 13:11:59 +0200

    feat(darwin): migrate mas to new module

--- modules/environment/homebrew.nix
+++ modules/environment/homebrew.nix
@@ -55,36 +55,6 @@
         # keep-sorted end
       ];
 
-      # mas list
-      masApps = {
-        # keep-sorted start
-        ausweisapp = 948660805;
-        base = 6744867438;
-        bitwarden = 1352778147;
-        dropover = 1355679052;
-        gapplin = 768053424;
-        home-assistant = 1099568401;
-        keka = 470158793;
-        mela = 1568924476;
-        microsoft-excel = 462058435;
-        microsoft-onedrive = 823766827;
-        microsoft-powerpoint = 462062816;
-        microsoft-word = 462054704;
-        parallels = 1085114709;
-        pdf-expert = 1055273043;
-        # pdf-presenter = 6472296409;
-        qr-factory = 1609285899;
-        reeder = 6475002485;
-        safari-1password = 1569813296;
-        safari-kagi = 1622835804;
-        safari-raindropio = 1549370672;
-        step-two = 1448916662;
-        testflight = 899247664;
-        todoist = 585829637;
-        whatsapp = 310633997;
-        # keep-sorted end
-      };
-
       caskFonts = map (name: "font-${name}") [
         # keep-sorted start
         "big-shoulders-display"
@@ -179,7 +149,6 @@
         taps = [ ];
         brews = [ ];
         casks = caskApps ++ caskFonts;
-        inherit masApps;
       };
     };
 }
---
+++ modules/programs/mas.nix
@@ -0,0 +1,38 @@
+{
+  flake.modules.darwin.default = {
+    programs.mas = {
+      enable = true;
+      update = true;
+      cleanup = true;
+      # mas list
+      # pdf-presenter = 6472296409;
+      packages = {
+        # keep-sorted start
+        ausweisapp = 948660805;
+        base = 6744867438;
+        bitwarden = 1352778147;
+        dropover = 1355679052;
+        gapplin = 768053424;
+        home-assistant = 1099568401;
+        keka = 470158793;
+        mela = 1568924476;
+        microsoft-excel = 462058435;
+        microsoft-onedrive = 823766827;
+        microsoft-powerpoint = 462062816;
+        microsoft-word = 462054704;
+        parallels = 1085114709;
+        pdf-expert = 1055273043;
+        qr-factory = 1609285899;
+        reeder = 6475002485;
+        safari-1password = 1569813296;
+        safari-kagi = 1622835804;
+        safari-raindropio = 1549370672;
+        step-two = 1448916662;
+        testflight = 899247664;
+        todoist = 585829637;
+        whatsapp = 310633997;
+        # keep-sorted end
+      };
+    };
+  };
+}
