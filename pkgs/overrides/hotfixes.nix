d83adbf7792ff33e4c212d40354c71b17f620cd8
Mirko Lenz <mirko@mirkolenz.com>
2026-08-24 17:28:20 +0200

    chore(pkgs/hotfixes): add tmux override

--- pkgs/overrides/hotfixes.nix
+++ pkgs/overrides/hotfixes.nix
@@ -3,4 +3,9 @@ final: prev:
 // (prev.lib.optionalAttrs prev.stdenv.hostPlatform.isLinux {
 })
 // (prev.lib.optionalAttrs prev.stdenv.hostPlatform.isDarwin {
+  # https://github.com/NixOS/nixpkgs/pull/555604
+  tmux = prev.tmux.overrideAttrs (prevAttrs: {
+    buildInputs = prevAttrs.buildInputs ++ [ prev.jemalloc ];
+    configureFlags = prevAttrs.configureFlags ++ [ "--enable-jemalloc" ];
+  });
 })
