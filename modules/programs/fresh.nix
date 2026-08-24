bca8e07c01a961fc997657e1b87fcb561566789f
Mirko Lenz <mirko@mirkolenz.com>
2026-08-24 08:16:17 +0200

    chore(fresh): update settings

--- modules/programs/fresh.nix
+++ modules/programs/fresh.nix
@@ -5,7 +5,28 @@
       programs.fresh-editor = {
         enable = true;
         package = pkgs.fresh-editor-bin;
-        settings = { };
+        settings = {
+          theme = "builtin://dark";
+          check_for_updates = false;
+          active_keybinding_map = "vscode";
+          file_browser = {
+            show_hidden = true;
+          };
+          file_explorer = {
+            show_hidden = true;
+          };
+          editor = {
+            completion_popup_auto_show = true;
+            enable_semantic_tokens_full = true;
+            ensure_final_newline_on_save = true;
+            indentation_guide = "all";
+            nerd_font_icons = true;
+            trim_trailing_whitespace_on_save = true;
+          };
+          plugins = {
+            git_explorer.settings.colorNames = true;
+          };
+        };
       };
     };
 }
