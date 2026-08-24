efec5a46c73d2df635b893f4868447b0463616cd
Mirko Lenz <mirko@mirkolenz.com>
2026-08-24 17:11:53 +0200

    chore(deps/pkgs): update
    Package updates:
    
    - herdrPlugins-automatic-rename: 0.7.1 -> 0.7.3
    - opencode-bin: 1.18.21 -> 1.18.22
    - pi-agent-bin: 0.84.2 -> 0.84.3

--- pkgs/by-name/herdrPlugins/automatic-rename.nix
+++ pkgs/by-name/herdrPlugins/automatic-rename.nix
@@ -11,14 +11,14 @@
 }:
 mkHerdrPlugin (finalAttrs: {
   pname = "herdr-automatic-rename";
-  version = "0.7.1";
+  version = "0.7.3";
   pluginId = "herdr-automatic-rename";
 
   src = fetchFromGitHub {
     owner = "qu8n";
     repo = "herdr-automatic-rename";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-PSngsykgTtF2/QKX6WmajqAluI1/v50fynKC8sjPMhI=";
+    hash = "sha256-Dz7+FtcOOai2mj3R2OgA3T3rLc6Bufe0qf3bhEWoRow=";
   };
 
   interpreters = [ bashNonInteractive ];
