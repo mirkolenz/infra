2cf6b04071919b3e3d715161df0ee45738b55b89
Mirko Lenz <mirko@mirkolenz.com>
2026-08-31 09:20:26 +0200

    fix(llama-cpp): use load-mode

--- modules/services/llama-cpp.nix
+++ modules/services/llama-cpp.nix
@@ -15,8 +15,7 @@
           fit = "off";
           flash-attn = "on";
           kv-unified = false;
-          mlock = true;
-          mmap = false;
+          load-mode = "mlock";
           n-gpu-layers = "all";
           parallel = 1;
           reasoning-preserve = true;
