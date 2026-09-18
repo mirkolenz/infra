e944b1c54baf682a33b1f48810283472c99b41ce
Mirko Lenz <mirko@mirkolenz.com>
2026-09-18 14:11:17 +0200

    chore(deps/nix): update
    Flake lock file updates:
    
    • Updated input 'mistral-vibe':
        'github:mistralai/mistral-vibe/19b5b74' (2026-09-12)
      → 'github:mistralai/mistral-vibe/c069ffa' (2026-09-18)

--- flake.nix
+++ flake.nix
@@ -65,7 +65,7 @@
       };
     };
     mistral-vibe = {
-      url = "github:mistralai/mistral-vibe/v2.25.4";
+      url = "github:mistralai/mistral-vibe/v2.25.5";
       inputs = {
         nixpkgs.follows = "nixpkgs";
         pyproject-nix.follows = "pyproject-nix";
